9367f67eefa18e483278df2ba8b78e9db25a2a27
Wei Ping <w6liao@eng.ucsd.edu>
2014-11-11 08:46:02 +0000

    Integrate GCompiler to the tool chain
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/GCompiler@19278 2cf53259-273a-af40-bda6-69d31023b8ec

--- headerSyn.py
+++ headerSyn.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
 import argparse
+import sys
+import os
 from lxml import etree
 from ComponentCatalog import *
 from mako.template import Template
@@ -6,22 +10,27 @@ from mako.template import Template
 
 
 class GComponent(object):
-    __catalog_path = "../../Libraries/Components/Catalog/Components.cat"
-    __catalog = ComponentCatalog(__catalog_path)
+    # __catalog_path = "../../Libraries/Components/Catalog/Components.cat"
+    # __catalog = ComponentCatalog(__catalog_path)
 
-    def __init__(self, e):
+    def __init__(self, e, catalog_path):
         self.var_name = e.get("progname")
         self.type = e.get("type")
+        self._catalog_path = catalog_path
+        self._catalog = ComponentCatalog(self._catalog_path)
 
-        catalog_element = GComponent.__catalog.getItems()[self.type]
-
-        self.class_name = catalog_element.find("API/arduino/class").get("name")
-        self.include_files = [include.get("file") for include in catalog_element.findall("API/arduino/include")]
+        try:
+            catalog_element = self._catalog.getItems()[self.type]
+            self.class_name = catalog_element.find("API/arduino/class").get("name")
+            self.include_files = [include.get("file") for include in catalog_element.findall("API/arduino/include")]
+        except Exception as e:
+            print e
+            sys.exit(-1)
 
 
 def generate_header_file(header_name, g_components):
 
-    mytemplate = Template(filename='./headertemplate.txt')
+    mytemplate = Template(filename=os.path.dirname(os.path.realpath(__file__)) + '/headertemplate.txt')
 
     flatten_include_files = []
     for component in g_components:
@@ -33,10 +42,14 @@ def generate_header_file(header_name, g_components):
     class_names = [component.class_name for component in g_components]
     var_names = [component.var_name for component in g_components]
 
-    print mytemplate.render(header_name=header_name,
-                            include_files=flatten_include_files,
-                            class_names=class_names,
-                            var_names=var_names)
+    file_text =  mytemplate.render(header_name=header_name,
+                                   include_files=flatten_include_files,
+                                   class_names=class_names,
+                                   var_names=var_names)
+
+    return file_text
+
+    # print("a file has been written to " + header_name)
 
     # string = ""
     #
@@ -55,18 +68,25 @@ def generate_header_file(header_name, g_components):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Tool for generating .h file for Arduino given .gspec file")
+    parser.add_argument("header_name")
     parser.add_argument("gspec")
+    parser.add_argument("catalog")
     args = parser.parse_args()
 
     gspec_path = args.gspec
+    catalog_path = args.catalog
+    header_name = args.header_name
     tree = etree.parse(gspec_path)
     root = tree.getroot()
 
-    purified_gadget_name = "_".join(root.findtext("name").split())
+    # purified_gadget_name = "_".join(root.findtext("name").split())
 
     g_components = []
 
     for element in root.iter("component"):
-        g_components.append(GComponent(element))
+        g_components.append(GComponent(element, catalog_path))
 
-    generate_header_file(purified_gadget_name, g_components)
+    file_text = generate_header_file(header_name, g_components)
+    f = open(header_name, 'w')
+    f.write(file_text)
+    f.close()
