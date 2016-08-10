f71112ffebb00536f8eb305f4e9f51226d1dab33
Steven Swanson <swanson@cs.ucsd.edu>
2016-08-10 10:52:09 -0700

    new files and explicit package

--- setup.py
+++ setup.py
@@ -10,10 +10,16 @@ with open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 with open(os.path.join(here, 'VERSION.txt'), encoding='utf-8') as f:
     version = f.read()
-
-setup(name=os.path.split(here)[1],
+    
+setup(name="GCompiler",
       version=version,
       long_description=long_description,
       author="NVSL, University of California San Diego",
-      packages = find_packages()
+      packages = find_packages(),
+      package_data={
+          "" : ["*.mako"]
+      },
+      include_package_data=True,
+      
+      
 )
